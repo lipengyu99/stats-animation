88adbef596949f98d818a5da8011b46cad0ddc0a
lipengyu2019 <lpy_cf@126.com>
2020-02-28 15:59:03 +0800

    first

---
+++ web/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <action-button/>
+    <feed-list/>
+  </div>
+</template>
+
+<script>
+import ActionButton from '../components/ActionButton.vue'
+import FeedList from '../components/FeedList.vue'
+export default {
+  components:{
+    "action-button":ActionButton,
+    "feed-list":FeedList,
+  }
+}
+</script>
+
+<style>
+.home{
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  background-color: #f1f2fb;
+}
+.heading{
+    padding: 16px 30px;
+    font-size: 18px;
+    font-weight: 600;
+}
+</style>
